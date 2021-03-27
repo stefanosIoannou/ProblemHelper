18e0262e55b870da09a48520f8eb471c863484b1
Stefanos Ioannou <stefanos.kestan@gmail.com>
2021-03-27 17:28:29 +0200

    Implement item command Implement init command

--- app/src/main/java/ProblemHelper/App.java
+++ app/src/main/java/ProblemHelper/App.java
@@ -14,7 +14,7 @@ public class App {
             Scanner sc = new Scanner(System.in);
             System.out.println("Enter a command: ");
             String str = sc.nextLine(); // reads string
-            Thread execution = (new Thread(){
+            Thread execution = (new Thread() {
 
                 @Override
                 public void run() {
@@ -34,16 +34,58 @@ public class App {
                         } else if (command.equals("close")) {
                             terminate = true;
                             System.out.println("Bye bye");
-                        } else {
+
+                            // Print Item Configuration
+                        } else if (command.equals("item")) {
+                            String toPrint = "Not found\n";
+                            Item itemOfThis = null;
+
+                            try {
+                                switch (Item.valueOf(commands[1])) {
+                                case ONE_ONE_ONE:
+                                    itemOfThis = Item.ONE_ONE_ONE;
+                                    break;
+                                case ONE_ONE_THREE:
+                                    itemOfThis = Item.ONE_ONE_THREE;
+                                    break;
+                                case ONE_ONE_TWO:
+                                    itemOfThis = Item.ONE_ONE_TWO;
+                                    break;
+                                case ONE_TWO_THREE:
+                                    toPrint = Item.ONE_TWO_THREE.toString();
+                                    itemOfThis = Item.ONE_TWO_THREE;
+                                    break;
+                                case ONE_TWO_TWO:
+                                    itemOfThis = Item.ONE_TWO_TWO;
+                                    break;
+                                case TWO_TWO_TWO:
+                                    itemOfThis = Item.TWO_TWO_TWO;
+                                    break;
+                                default:
+                                    break;
+                                }
+                            } catch (Exception e) {
+
+                            }
+
+                            if (itemOfThis != null) {
+                                toPrint = itemOfThis.toString();
+                            }
+                            System.out.println(toPrint);
+                            if (commands.length > 2)
+                                System.out.println(Item.assignOrientation(itemOfThis, Integer.valueOf(commands[2])));
+
+                        } else if (command.equals("init")) {
+                            System.out.println("(define (problem another_problem)\n(:domain example)\n;(:situation <situation_name>) ;deprecated\n(:objects\n\n)\n(:init\n\n)\n(:goal\n(and\n\n)\n)\n)\n\n");
+                        }else {
                             System.out.println("Invalid command");
                         }
                     }
                 }
-                
+
             });
             execution.start();
             execution.join();
         }
     }
 }
-
---
+++ app/src/main/java/ProblemHelper/Item.java
@@ -0,0 +1,38 @@
+package ProblemHelper;
+
+public enum Item {
+    ONE_ONE_ONE("one_one_one", 1), TWO_TWO_TWO("two_two_two", 1), ONE_ONE_THREE("one_one_three", 2),
+    ONE_TWO_TWO("one_two_two", 2), ONE_ONE_TWO("one_one_two", 2), ONE_TWO_THREE("one_two_three", 6),
+    UNKOWN("UNKNOWN", 2);
+
+    private final String stringName;
+    private final int noOfOrientations;
+    private static final String[] orientations = new String[] { "zero", "one", "two", "three", "four", "five" };
+
+    private Item(String str, int noOfOrientations) {
+        stringName = str;
+        this.noOfOrientations = noOfOrientations;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        builder.append("\n;Item\n").append("item - ").append(stringName);
+        builder.append("\n ; Total Orientations\n").append("(= (no_orientations item) " + noOfOrientations + ")");
+        builder.append("\n");
+
+        // If the object has orientations, print all orientations
+        for (int i = 0; i < noOfOrientations; ++i) {
+            builder.append("\n").append("(= (orientation_no " + orientations[i] + ") " + i + ")");
+        }
+        return builder.toString();
+    }
+
+    public static String assignOrientation(Item item, int orientationIndex) {
+        if (orientationIndex < item.noOfOrientations) {
+            return "\n(has_orientation " + item.stringName + " " + orientations[orientationIndex] + ")\n";
+        }
+        return "";
+    }
+
+}
